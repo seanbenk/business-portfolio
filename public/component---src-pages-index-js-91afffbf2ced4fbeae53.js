fbd817d08a2e0d1a2beb17a7010f587a36152c6d
seanbenk <76204079+seanbenk@users.noreply.github.com>
2021-10-04 02:49:20 +1100

    added public folder

---
+++ public/component---src-pages-index-js-91afffbf2ced4fbeae53.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkportfolio=self.webpackChunkportfolio||[]).push([[678],{7704:function(e,o,r){r.r(o);var t=r(7294),n={backgroundColor:r(6575).Z.primary,fontFamily:"-apple-system, Roboto, sans-serif, serif",margin:0};o.default=function(){return t.createElement("main",{style:n},"Hello")}}}]);
+//# sourceMappingURL=component---src-pages-index-js-91afffbf2ced4fbeae53.js.map
