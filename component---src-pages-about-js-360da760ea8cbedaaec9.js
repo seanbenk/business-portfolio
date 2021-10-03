101a82da345ed06b1f32e8a63a81f530c96f29ed
seanbenk <76204079+seanbenk@users.noreply.github.com>
2021-10-04 02:30:18 +1100

    Updates

---
+++ component---src-pages-about-js-360da760ea8cbedaaec9.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkportfolio=self.webpackChunkportfolio||[]).push([[682],{5518:function(e,t,u){u.r(t);var n=u(7294);t.default=function(e){return n.createElement("div",null,"the about page!")}}}]);
+//# sourceMappingURL=component---src-pages-about-js-360da760ea8cbedaaec9.js.map
---
+++ component---src-pages-about-js-f8c17a075969afd0a6fc.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkportfolio=self.webpackChunkportfolio||[]).push([[682],{8383:function(e,t,n){n.r(t),n.d(t,{default:function(){return u}});var o=n(7294);var u=function(e){return o.createElement("div",{className:"about-module--container--31sWA"},"the about page!")}}}]);
+//# sourceMappingURL=component---src-pages-about-js-f8c17a075969afd0a6fc.js.map
