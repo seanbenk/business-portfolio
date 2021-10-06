45edb0dd65e9882d9ecb4e670a91648ffed3567e
seanbenk <76204079+seanbenk@users.noreply.github.com>
2021-10-06 14:27:36 +1100

    updates to nav menu on mobile

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { isMobile } from "react-device-detect";
 
 import A from "./A";
 import * as styles from "./Nav.module.scss";
 
 function Nav(props) {
+  const [device, setDevice] = useState();
+  useEffect(() => {
+    setDevice(isMobile ? "mobile" : "desktop");
+  }, []);
   return (
     <div
       style={{
